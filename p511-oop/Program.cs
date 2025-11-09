7e0d42f75f8b6113551525c3d2cdbfd74bf1e435
Artem <artm.breslavskiy@gmail.com>
2025-11-09 14:36:13 +0500

    Task Complited

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -8,6 +8,11 @@ namespace p511_oop
 {
     internal class Program
     {
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
         static void Main(string[] args)
         {
         }
