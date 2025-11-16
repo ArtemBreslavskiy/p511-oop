cb11f98cf743d46db94623e90cdb4a7ea543cfaf
Artem <artm.breslavskiy@gmail.com>
2025-11-16 14:19:35 +0500

    Задание выполнено

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,70 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Rectangle rect1 = new Rectangle(3, 4.5);
+
+            Console.WriteLine($"Ширина: {rect1.Width}");
+            Console.WriteLine($"Высота: {rect1.Height}");
+            Console.WriteLine($"Площадь: {rect1.Area}");
+            Console.WriteLine($"Периметр: {rect1.Perimeter}");
+        }
+
+        class Rectangle
+        {
+            private double _width;
+            private double _height;
+
+            public double Width
+            {
+                get
+                {
+                    return _width;
+                }
+                set
+                {
+                    if (value > 0)
+                    {
+                        _width = value;
+                    }
+                }
+            }
+
+            public double Height
+            {
+                get
+                {
+                    return _height;
+                }
+                set
+                {
+                    if (value > 0)
+                    {
+                        _height = value;
+                    }
+                }
+            }
+
+            public double Area
+            {
+                get
+                {
+                    return _width * _height;
+                }
+            }
+
+            public double Perimeter
+            {
+                get
+                {
+                    return (_width + _height) * 2;
+                }
+            }
+
+            public Rectangle(double width, double height)
+            {
+                Width = width;
+                Height = height;
+            }
         }
     }
 }
