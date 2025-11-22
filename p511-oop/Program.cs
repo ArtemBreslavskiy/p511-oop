f88a26cbc5ea73f7c81f552ead0a3f51ad733ceb
Artem <artm.breslavskiy@gmail.com>
2025-11-22 14:31:19 +0500

    Task 9 is completed

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +10,33 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
-            
+            int[] arr1 = { 1, 2, 3, 4 };
+            int[] arr2 = { 3, 10, 9, 3 };
+            int[] arr3 = { 2, 7 };
+
+            PrintArray(ArraysSum(arr1, arr3));
+        }
+
+        static int[] ArraysSum(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+            {
+                throw new ArgumentException("Неверная длинна массивов. Длинна массивов должна быть одинаковой.");
+            }
+
+            int[] result = new int[arr1.Length];
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                result[i] = arr1[i] + arr2[i];
+            }
+            return result;
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine($"{i + 1}: {arr[i]}");
         }
     }
 }
