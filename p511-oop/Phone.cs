263d63a5cd6f2599ded874ebf200775929b09de3
Artem <artm.breslavskiy@gmail.com>
2025-11-23 14:53:54 +0500

    Phone

---
+++ p511-oop/Phone.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p511_oop
+{
+    internal class Phone
+    {
+        private string brand;
+        private string model;
+        private decimal price;
+
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+            private set
+            {
+                brand = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            private set
+            {
+                model = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public Phone(string brand = "Unknown", string model = "Unknown", decimal price = -1)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.price = price;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\nBrand: {brand}");
+            Console.WriteLine($"Model: {model}");
+            Console.WriteLine($"Price: {price}");
+        }
+    }
+}
--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -11,7 +11,12 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
-            
+            Phone phone1 = new Phone("Apple", "IPhone 13 Pro", 85000);
+            Phone phone2 = new Phone("Samsung", "Galaxy S25", 77000);
+
+            phone1.ShowInfo();
+            phone2.ShowInfo();
+
         }
     }
 }
