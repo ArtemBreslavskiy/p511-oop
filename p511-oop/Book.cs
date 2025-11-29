4ebd0d4712ccc3eee1057917f4182378110bdebe
Artem <artm.breslavskiy@gmail.com>
2025-11-29 14:55:13 +0500

    Uncomplite

---
+++ p511-oop/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p511_oop
+{
+    internal class Book
+    {
+        public string Title {  get; private set; }
+        public string Author {  get; private set; }
+        public string Publisher {  get; private set; }
+        public int YearOfPrinting {  get; private set; }
+        public int PagesCount {  get; private set; }
+
+        public enum bookArguments
+        {
+            Title,
+            Author,
+            Publisher,
+            YearOfPrinting,
+            PagesCount
+        }
+
+
+        public Book(string title = "Unknown", string author = "Unknown", string publisher = "Unknown", int yearOfPrinting = -1, int pagesCount = -1)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            YearOfPrinting = yearOfPrinting;
+            PagesCount = pagesCount;
+        }
+
+        public void PrintFilteredData(bookArguments argument, string value)
+        {
+            if (argument == bookArguments.YearOfPrinting || argument == bookArguments.PagesCount)
+            {
+                int intValue = Convert.ToInt32(value);
+            }
+        }
+    }
+}
--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -11,7 +11,16 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
-            
+            Book[] books = {
+                new Book("George Orwell", "1984", "Penguin Books", 1949, 328),
+                new Book("J.K. Rowling", "Harry Potter", "Bloomshury", 1997, 223),
+                new Book(),
+                new Book(),
+                new Book(),
+                new Book(),
+                new Book(),
+                new Book()
+            };
         }
     }
 }
