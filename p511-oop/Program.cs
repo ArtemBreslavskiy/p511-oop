ff76fbd1304cd6ccbb5776cb7ccf02bd5afc7ee0
Artem <artm.breslavskiy@gmail.com>
2026-01-11 14:41:09 +0500

    /

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Deployment.Internal;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
@@ -12,6 +13,27 @@ namespace p511_oop
         static void Main(string[] args)
         {
             
+        }
+
+        static double Mv(double[,] m, double[] v)
+        {
+            double[] answer = new double[m.GetLength(1)];
+            return - 1;
+        }
+
+        static int Activation(double x)
+        {
+            if (x > 0.5) return 1;
+            else return 0;
+        }
+
+        static int Go(int sun, int rain, int cold)
+        {
+            int[] X = new int[] { sun, rain, cold};
+            double[,] Wh = new double[,] { { 0.3, 0.3, 0 }, { -0.3, 0.2, 0.5 } };
+            double[] Wout = new double[] { 1, -1 };
+
+            return -1;
         }
     }
 }
