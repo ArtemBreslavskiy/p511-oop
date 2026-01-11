275f737be4d67fbb7c5977e8044e6d9390a419bf
Artem <artm.breslavskiy@gmail.com>
2026-01-11 14:53:14 +0500

    /

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,14 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
-            
+            const string URL = "https://jsonplaceholder.org/posts";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            var response = client.GetAsync(URL);
+
+            Console.WriteLine(response.Result);
         }
     }
 }
